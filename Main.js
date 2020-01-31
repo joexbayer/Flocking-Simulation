c0c0f340477ae1878f233b15162a814a7e3f9cf8
Joe Bayer <joeba@uio.no>
2020-01-31 20:13:22 +0100

    optimized main javascript

--- Main.js
+++ Main.js
@@ -169,138 +169,8 @@ class TeamBot extends Bot {
 		ctx.lineTo((this.position_x + this.velx*20), (this.position_y + this.vely*20));
 		ctx.stroke();
 	}
-
-	formation(bots, ctx){
-		var perception = 80;
-		var botxy = [this.position_x, this.position_y];
-		var total = 0;
-
-		//for every bot
-		for (var p = 0; p < bots.length; p++) {
-			//not current bot
-			if(this != bots[p]){
-				//get next bots posistion
-				var next_botxy = bots[p].posBot();
-				var avg_vector = [0,0];
-				//get distance vector of current bot to next bot
-				var distance_vector = [botxy[0]-next_botxy[0], botxy[1]-next_botxy[1]];
-				//calculate distance between current bot and next bot with distance vector
-				var distance = Math.floor(Math.sqrt( distance_vector[0]*distance_vector[0] + distance_vector[1]*distance_vector[1]));
-				//if same team and if distance is less then given perception
-				if((this.teamid == bots[p].getTeamID()) && (distance < perception)){
-					ctx.beginPath();
-					ctx.strokeStyle = "light"+this.team;
-					ctx.moveTo(this.position_x, this.position_y);
-					ctx.lineTo((next_botxy[0]), (next_botxy[1]));
-					ctx.stroke();
-				}
-			}
-		}
-	}
-	//cohesion, get average position of bots. Get vector to that point and set acceleration 1/180 of position vector.
-	cohesion(bots){
-		var perception = 80;
-		var total = 1;
-		var avg_vector = [this.position_x, this.position_y];
-		var botxy = [this.position_x, this.position_y];
-
-		for (var p = 0; p < bots.length; p++) {
-			if(this != bots[p]){
-				var next_botxy = bots[p].posBot();
-				var distance_vector = [botxy[0]-next_botxy[0], botxy[1]-next_botxy[1]];
-				var distance = Math.floor(Math.sqrt( distance_vector[0]*distance_vector[0] + distance_vector[1]*distance_vector[1]));
-				if(((this.teamid == bots[p].getTeamID() && bots[p].getTeamID() != 3)) && (distance < perception) && (distance > 30)){
-					avg_vector = [avg_vector[0]+bots[p].posBot()[0], avg_vector[1]+bots[p].posBot()[1]];
-					total++;
-				}
-				if(((this.teamid == 3 && bots[p].getTeamID() != 3)) && (distance < perception) && (distance > 30)){
-					avg_vector = [avg_vector[0]+bots[p].posBot()[0], avg_vector[1]+bots[p].posBot()[1]];
-					total++;
-				}
-			}
-
-		}
-		if (total > 1 && total < 7){
-			if(this.teamid ==3){
-				avg_vector = [(avg_vector[0]/total)-botxy[0], (avg_vector[1]/total)-botxy[1]];
-				this.acc(avg_vector[0]/10, avg_vector[1]/10);
-			} else {
-				avg_vector = [(avg_vector[0]/total)-botxy[0], (avg_vector[1]/total)-botxy[1]];
-				this.acc(avg_vector[0]/180, avg_vector[1]/180);//150
-			}
-
-		}
-
-	}
-
-	/*speration, get distance vector of bots in perception, if they are inside of perception create a acceleration vector in the opposite direction
-	of the distance vector, 1/25.
-	*/
-	seperation(bots){
-		var perception = 20;
-		var avg_vector = [0,0];
-		var botxy = [this.position_x, this.position_y];
-		var total = 1;
-		for (var p = 0; p < bots.length; p++) {
-			if(this != bots[p]){
-				var next_botxy = bots[p].posBot();
-				var distance_vector = [botxy[0]-next_botxy[0], botxy[1]-next_botxy[1]];
-
-				var distance = Math.floor(Math.sqrt( distance_vector[0]*distance_vector[0] + distance_vector[1]*distance_vector[1]));
-				if((this.teamid == bots[p].getTeamID()) && (distance < perception)){
-					var temp_x = -(bots[p].posBot()[0]-botxy[0]);
-					var temp_y = -(bots[p].posBot()[1]-botxy[1]);
-
-					avg_vector = [avg_vector[0]+temp_x, avg_vector[1]+temp_y];
-					total++;
-				}
-			}
-		}
-		if (total > 1){
-			if(this.teamid ==3){
-				avg_vector = [(avg_vector[0]/total), (avg_vector[1]/total)];
-				this.acc(avg_vector[0], avg_vector[1]);
-			} else {
-				avg_vector = [(avg_vector[0]/total), (avg_vector[1]/total)];
-				this.acc(avg_vector[0]/25, avg_vector[1]/25);
-			}
-		}
-
-	}
-	//flee, if bots of different team, create a high acceleration from the distance vector in the opposite direction.
-	flee(bots){
-		var perception = 30;
-		var avg_vector = [0,0];
-		var botxy = [this.position_x, this.position_y];
-		var total = 1;
-		for (var p = 0; p < bots.length; p++) {
-			if(this != bots[p]){
-				var next_botxy = bots[p].posBot();
-				var distance_vector = [botxy[0]-next_botxy[0], botxy[1]-next_botxy[1]];
-
-				var distance = Math.floor(Math.sqrt( distance_vector[0]*distance_vector[0] + distance_vector[1]*distance_vector[1]));
-				if((this.teamid == 3 && bots[p].getTeamID() != 3) && distance < 15){
-						bots[p].changeTeam(this.teamid, this.team);
-						console.log("test");
-						return;
-					}
-				if((this.teamid != bots[p].getTeamID()) && (distance < perception) && this.teamid != 3){
-					var temp_x = -(bots[p].posBot()[0]-botxy[0]);
-					var temp_y = -(bots[p].posBot()[1]-botxy[1]);
-
-					avg_vector = [avg_vector[0]+temp_x, avg_vector[1]+temp_y];
-					total++;
-				}
-			}
-		}
-		if (total > 1){
-			avg_vector = [(avg_vector[0]/total), (avg_vector[1]/total)];
-			this.acc(avg_vector[0]/5, avg_vector[1]/5);
-		}
-
-	}
 	//fleeMouse, same as flee, but from the mouse x, y coordiantes.
-	fleeMouse(mouse_x,mouse_y, ctx){
+	fleeMouse(mouse_x,mouse_y){
 		var perception = 60;
 		var avg_vector = [0, 0];
 		var botxy = [this.position_x, this.position_y];
@@ -330,11 +200,27 @@ class TeamBot extends Bot {
 
 	/* Align, get the average velocity vector from bots in the perception radius. Then create acceleration vector from the average one, 1/35
 	*/
-	align(bots){
-		var perception = 60;
-		var avg_vector = [0,0];
+	align(bots, doFormation, ctx){
+		//align
+		var Aperception = 60;
+		var align_avg_vector = [0,0];
 		var botxy = [this.position_x, this.position_y];
-		var total = 1;
+		var Atotal = 1;
+
+		//flee
+		var Fperception = 30;
+		var flee_avg_vector = [0,0];
+		var Ftotal = 1;
+
+		//seperate
+		var Sperception = 20;
+		var sep_avg_vector = [0,0];
+		var Stotal = 1;
+
+		//cohesion
+		var Cperception = 80;
+		var Ctotal = 1;
+		var coh_avg_vector = [this.position_x, this.position_y];
 
 		for (var p = 0; p < bots.length; p++) {
 			if(this != bots[p]){
@@ -342,20 +228,88 @@ class TeamBot extends Bot {
 				var distance_vector = [botxy[0]-next_botxy[0], botxy[1]-next_botxy[1]]
 
 				var distance = Math.floor(Math.sqrt( distance_vector[0]*distance_vector[0] + distance_vector[1]*distance_vector[1]));
-				if((this.teamid == bots[p].getTeamID() && this.teamid != 3) && (distance < perception)){
-					avg_vector = [avg_vector[0]+bots[p].velBot()[0], avg_vector[1]+bots[p].velBot()[1]];
-					total++;
+				//align
+				if((this.teamid == bots[p].getTeamID() && this.teamid != 3) && (distance < Aperception)){
+					align_avg_vector = [align_avg_vector[0]+bots[p].velBot()[0], align_avg_vector[1]+bots[p].velBot()[1]];
+					Atotal++;
+				}
+				//flee
+				if((this.teamid == 3 && bots[p].getTeamID() != 3) && distance < 15){
+						bots[p].changeTeam(this.teamid, this.team);
+						console.log("test");
+						return;
+					}
+				if((this.teamid != bots[p].getTeamID()) && (distance < Fperception) && this.teamid != 3){
+					var temp_x = -(bots[p].posBot()[0]-botxy[0]);
+					var temp_y = -(bots[p].posBot()[1]-botxy[1]);
+
+					flee_avg_vector = [flee_avg_vector[0]+temp_x, flee_avg_vector[1]+temp_y];
+					Ftotal++;
+				}
+				//seperate
+				if((this.teamid == bots[p].getTeamID()) && (distance < Sperception)){
+					var temp_x = -(bots[p].posBot()[0]-botxy[0]);
+					var temp_y = -(bots[p].posBot()[1]-botxy[1]);
+
+					sep_avg_vector = [sep_avg_vector[0]+temp_x, sep_avg_vector[1]+temp_y];
+					Stotal++;
+				}
+				//cohesion
+				if(((this.teamid == bots[p].getTeamID() && bots[p].getTeamID() != 3)) && (distance < Cperception) && (distance > 30)){
+					coh_avg_vector = [coh_avg_vector[0]+bots[p].posBot()[0], coh_avg_vector[1]+bots[p].posBot()[1]];
+					Ctotal++;
+				}
+				if(((this.teamid == 3 && bots[p].getTeamID() != 3)) && (distance < Cperception) && (distance > 30)){
+					coh_avg_vector = [coh_avg_vector[0]+bots[p].posBot()[0], coh_avg_vector[1]+bots[p].posBot()[1]];
+					Ctotal++;
+				}
+				//formation
+				if(doFormation){
+					if((this.teamid == bots[p].getTeamID()) && (distance < Cperception)){
+						ctx.beginPath();
+						ctx.strokeStyle = "light"+this.team;
+						ctx.moveTo(this.position_x, this.position_y);
+						ctx.lineTo((next_botxy[0]), (next_botxy[1]));
+						ctx.stroke();
+					}
 				}
 			}
 		}
-		if (total > 1 && total < 8){
+		//align
+		if (Atotal > 1 && Atotal < 8){
 			if(this.teamid == 3){
-				avg_vector = [(avg_vector[0]/total), (avg_vector[1]/total)];
+				align_avg_vector = [(align_avg_vector[0]/Atotal), (align_avg_vector[1]/Atotal)];
 				this.acc(avg_vector[0], avg_vector[1]);
 			} else {
-				avg_vector = [(avg_vector[0]/total), (avg_vector[1]/total)];
-				this.acc(avg_vector[0]/35, avg_vector[1]/35);//25
+				align_avg_vector = [(align_avg_vector[0]/Atotal), (align_avg_vector[1]/Atotal)];
+				this.acc(align_avg_vector[0]/35, align_avg_vector[1]/35);//25
 			}
+		}
+		//Flee
+		if (Ftotal > 1){
+			flee_avg_vector = [(flee_avg_vector[0]/Ftotal), (flee_avg_vector[1]/Ftotal)];
+			this.acc(flee_avg_vector[0]/5, flee_avg_vector[1]/5);
+		}
+		//seperate
+		if (Stotal > 1){
+			if(this.teamid ==3){
+				sep_avg_vector = [(sep_avg_vector[0]/Stotal), (sep_avg_vector[1]/Stotal)];
+				this.acc(sep_avg_vector[0], sep_avg_vector[1]);
+			} else {
+				sep_avg_vector = [(sep_avg_vector[0]/Stotal), (sep_avg_vector[1]/Stotal)];
+				this.acc(sep_avg_vector[0]/25, sep_avg_vector[1]/25);
+			}
+		}
+		//cohesion
+		if (Ctotal > 1 && Ctotal < 7){
+			if(this.teamid ==3){
+				coh_avg_vector = [(coh_avg_vector[0]/Ctotal)-botxy[0], (coh_avg_vector[1]/Ctotal)-botxy[1]];
+				this.acc(coh_avg_vector[0]/10, coh_avg_vector[1]/10);
+			} else {
+				coh_avg_vector = [(coh_avg_vector[0]/Ctotal)-botxy[0], (coh_avg_vector[1]/Ctotal)-botxy[1]];
+				this.acc(coh_avg_vector[0]/180, coh_avg_vector[1]/180);//150
+			}
+
 		}
 	}
 
@@ -474,8 +428,8 @@ function activateFormation(){
 
 //update function running rules.
 setInterval(function update(){
-	ctx.fillStyle = "black";
-	ctx.fillRect(0, 0, 1000, 1000);
+	ctx.fillStyle = "#212121";
+	ctx.fillRect(0, 0, screen_width, screen_height);
 	var totalHunter = 0;
 
 	for (var p = 0; p < bots.length; p++) {
@@ -487,13 +441,7 @@ setInterval(function update(){
 			bots[p].drawDetails(ctx);
 		}
 		bots[p].draw(ctx);
-		if(showFormation){
-			bots[p].formation(bots, ctx);
-		}
 		bots[p].move(screen_width, screen_height);
-		bots[p].cohesion(bots);
-		bots[p].seperation(bots);
-		bots[p].align(bots);
-		bots[p].flee(bots);
+		bots[p].align(bots, showFormation, ctx);
 	}
 }, 30);
